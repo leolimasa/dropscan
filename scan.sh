d0ae7cb224cee6dfb14b27ac4847feed11425b15
Leonardo Sa <leo@leo-sa.com>
2019-06-09 12:09:57 -0500

    Completed first version of scan.sh

--- scan.sh
+++ scan.sh
@@ -1,4 +1,5 @@
-#!/bin/bashs
+#!/bin/bash
+#set -x
 
 # REQUIRES:
 #
@@ -7,30 +8,57 @@
 # tesseract-ocr
 # tesseract-ocr-eng
 
+#TEMP_DIR=$(mktemp -d)
+PREFIX=$1
+
+if [ -z "${OUTPUT_DIR}" ]; then
+  OUTPUT_DIR=`pwd`
+fi
+
+# UNCOMMENT FOR TESTING
+mkdir processed
+cp *.pbm processed/
+TEMP_DIR=`pwd`/processed
+echo "Temp dir: $TEMP_DIR"
+echo "Output dir: $OUTPUT_DIR"
+
 # Scan the document
-scanadf -o $1-%d.pbm --source "Automatic Document Feeder(left aligned,Duplex)" --mode "24 bit Color" 
+#scanadf -o $TEMP_DIR/$PREFIX-%d.pbm --source "Automatic Document Feeder(left aligned,Duplex)" --mode "24 bit Color" 
 
-# Convert to PNG
-#for FILENAME in $(ls *.pbm); do convert -rotate 180 $FILENAME ${FILENAME%.*}.png ;done
-#rm *.pbm
+# Convert to PNG and rotate 180
+find $TEMP_DIR -name "$PREFIX-*.pbm" \
+  | xargs -I {} basename {} .pbm \
+  | xargs -P 4 -I {} convert -rotate 180 $TEMP_DIR/{}.pbm $TEMP_DIR/{}.png 
+rm $TEMP_DIR/*.pbm
 
+# Reverses the numeric order of the files. Needed because the output of
+# adfscan makes the first page scanned the last.
 reverse() {
+	PREFIX=$1
+	TEMP_DIR=$2
 	# create a temporary directory
-	mkdir -p ./tmp
+	mkdir -p $TEMP_DIR/reverse
 
 	# Reverse numbers
-	images=(*.png)
+	images=(`ls $TEMP_DIR/$PREFIX*.png | sort -V`)
 	max=${#images[*]}
 
 	# loop through array keys and subtract the key from maximum number to reverse
 	for i in "${!images[@]}"; do 
 	  # rename to the temporary directory, with three-digit zero padding
-	  mv -- "${images[$i]}" ./tmp/$(printf "$1-%03d.png" $(($max - i)))
+	  mv -- "${images[$i]}" $TEMP_DIR/reverse/$(printf "$PREFIX-%03d.png" $(($max - i)))
 	done
 
 	# move files back and remove temporary directory
-	mv ./tmp/*.png .
-	rmdir ./tmp
+	mv $TEMP_DIR/reverse/*.png $TEMP_DIR
+	rm -rf $TEMP_DIR/reverse
 }
 
-#reverse $1
+reverse $PREFIX $TEMP_DIR
+
+# Create the searchable pdf
+ls $TEMP_DIR/$PREFIX*.png | tesseract - "$OUTPUT_DIR/$(date "+%Y-%m-%d")-$PREFIX" pdf
+
+# Done!
+rm -rf $TEMP_DIR
+exit
